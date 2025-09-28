d75246e2ccb29cd319eeffbd9f05675e4e83c6a7
Antonio Fortin <antonio@kikodo.io>
2025-09-28 22:18:52 +0700

    feat: Initialize Django project with PostgreSQL configuration

---
+++ kikodo_crm/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('crm.urls')),
+    path('analytics/', include('analytics.urls')),
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
